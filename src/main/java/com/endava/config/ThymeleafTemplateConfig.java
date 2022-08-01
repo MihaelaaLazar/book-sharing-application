114f38f5f181e46fd9628c85f2d2b7617c170c0f
rolazar <rodica-mihaela.lazar@endava.com>
2022-08-01 11:22:30 +0300

    added thymeleaf dependency for email template and changed the credentials for database to allowed to connect to heroku

---
+++ src/main/java/com/endava/config/ThymeleafTemplateConfig.java
@@ -0,0 +1,21 @@
+package com.endava.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.thymeleaf.templatemode.TemplateMode;
+import org.thymeleaf.templateresolver.ClassLoaderTemplateResolver;
+
+import java.nio.charset.StandardCharsets;
+
+@Configuration
+public class ThymeleafTemplateConfig {
+
+    public ClassLoaderTemplateResolver emailTemplateResolver() {
+        ClassLoaderTemplateResolver emailTemplateResolver = new ClassLoaderTemplateResolver();
+        emailTemplateResolver.setPrefix("/templates/");
+        emailTemplateResolver.setSuffix(".html");
+        emailTemplateResolver.setTemplateMode(TemplateMode.HTML);
+        emailTemplateResolver.setCharacterEncoding(StandardCharsets.UTF_8.name());
+        emailTemplateResolver.setCacheable(false);
+        return emailTemplateResolver;
+    }
+}
